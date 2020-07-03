47d66379b93d6903f3811f3bce2b2d97bb989279
Alex <thealexvarga@gmail.com>
2020-07-03 02:07:06 -0400

    wrote setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
 
-print("Symlinking Config Files...")
+import os
 
-print("Psych, not implemented yet")
+####
+
+print("Symlinking Config Files...")
+print()
+
+def run(cmd):
+    print(f'SETUP : {cmd}')
+    os.system(cmd)
+
+rootdir = "files"
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        filepath = subdir + os.sep + file
+
+        cmd_mkdir = f'mkdir -p ~{subdir[5:]}'
+        cmd_rm = f'rm ~{filepath[5:]}'
+        cmd_ln = f'ln -s {filepath} ~{filepath[5:]}'
+
+        run(cmd_mkdir)
+        run(cmd_rm)
+        run(cmd_ln)
+
+print()
+print("Done. Logout+login to trigger the goodness.")
